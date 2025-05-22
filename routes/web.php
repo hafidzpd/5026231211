ac06b9131df8d9315ce16d56c96686af1cdb2662
hafidzpd <5026231211@student.its.ac.id>
2025-05-22 15:50:31 +0700

    pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 //import java.io ;
 
 //system.out.println("Hello World");
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('welcome', function () {
+//     return view('welcome');
+// });
 
 
 Route::get('halo', function () {
@@ -48,3 +51,13 @@ Route::get('frontend', function () {
 Route::get('index', function () {
     return view('index');
 });
+
+Route::get('dosen', [DosenController::class, 'index']);
+Route::get('welcome', [DosenController::class, 'welcome']);
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
